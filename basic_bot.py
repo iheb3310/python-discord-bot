f27341a865142102b7ea6314baf2c4c19b33605e
Stephen Park <stephenspark@utexas.edu>
2017-09-28 13:25:03 -0500

    Masked public token

--- basic_bot.py
+++ basic_bot.py
@@ -49,4 +49,4 @@ async def joined(member : discord.Member):
     """Says when a member joined."""
     await bot.say('{0.name} joined in {0.joined_at}'.format(member))
 
-bot.run('MzYzMDAyODk4NzEzNjczNzI4.DK64eg.hAsdc9t6xTRGLXXUNAMWCFBRP8I')
+bot.run('TOKEN')
