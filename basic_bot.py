fb00a2f3fb6765487ddf6fd911ef017c48a991b2
Stephen Park <stephenspark@utexas.edu>
2017-09-28 13:06:26 -0500

    Bot changes

--- basic_bot.py
+++ basic_bot.py
@@ -25,21 +25,17 @@ async def on_ready():
     print('------')
 
 @bot.command()
+async def helpme():
+    '''outputs list of commands'''
+    await bot.say('Current list of commands...')
+    await bot.say('Add: add two numbers together. Example: "!add 12 12"')
+    await bot.say('Choose: pick random from choices given. Example: "!choose eenie meenie minie moe"')
+    await bot.say('Joined: get join date of member. Example: "!joined RedRedemption"')
+
+@bot.command()
 async def add(left : int, right : int):
     """Adds two numbers together."""
     await bot.say(left + right)
-
-@bot.command()
-async def roll(dice : str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await bot.say('Format has to be in NdN!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-    await bot.say(result)
 
 @bot.command(description='For when you wanna settle the score some other way')
 async def choose(*choices : str):
@@ -50,18 +46,5 @@ async def choose(*choices : str):
 async def joined(member : discord.Member):
     """Says when a member joined."""
     await bot.say('{0.name} joined in {0.joined_at}'.format(member))
-
-@bot.group(pass_context=True)
-async def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await bot.say('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-@cool.command(name='bot')
-async def _bot():
-    """Is the bot cool?"""
-    await bot.say('Yes, the bot is cool.')
 
 bot.run('MzYzMDAyODk4NzEzNjczNzI4.DK64eg.hAsdc9t6xTRGLXXUNAMWCFBRP8I')
