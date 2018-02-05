71e0cf42e218a1d4d62fb2b2bb67a674b8c2422d
Stephen Park <ssp0929@gmail.com>
2018-02-04 21:58:44 -0600

    Ported over some features from my other bot.

--- basic_bot.py
+++ basic_bot.py
@@ -23,29 +23,184 @@ def initiate_bot():
         print(bot.user.id)
         print('------')
 
-    @bot.command()
-    async def helpme():
-        '''outputs list of commands'''
-        help_text = 'get list of commands. Example: "!helpme"'
-        add_text = 'add two numbers together. Example: "!add 12 12"'
-        subtract_text = 'subtract two numbers. Example: "!subtract 12 2"'
-        multiply_text = 'multiply two numbers together. Example: "!multiply 4 3"'
-        divide_text = 'divide two numbers. Example: "!divide 6 2"'
-        choose_text = 'pick random from choices given. Example: "!choose eenie meenie minie moe"'
-        repeat_text = 'get the bot to repeat some input. Example "!repeat I love life'
-        addquote_text = 'add quote to database. Example: "!addquote This is a quote.'
-        getquote_text = 'gets random quote from database. Example: "!getquote"'
+    @bot.command(pass_context=True)
+    async def multiply(ctx):
+        """multiplies two numbers together"""
+        left = float(ctx.message.content.split()[1])
+        right = float(ctx.message.content.split()[2])
+        header = str(left) + ' * ' + str(right)
+        text = str(left * right)
 
-        embed = discord.Embed(title="Commands", description="Current list of commands.")
-        embed.add_field(name='Help', value=help_text, inline=True)
-        embed.add_field(name='Add', value=add_text, inline=True)
-        embed.add_field(name='Subtract', value=subtract_text, inline=True)
-        embed.add_field(name='Multiply', value=multiply_text, inline=True)
-        embed.add_field(name='Divide', value=divide_text, inline=True)
-        embed.add_field(name='Choose', value=choose_text, inline=True)
-        embed.add_field(name='Repeat', value=repeat_text, inline=True)
-        embed.add_field(name='Add Quote', value=addquote_text, inline=True)
-        embed.add_field(name='Get Quote', value=getquote_text, inline=True)
+        embed = discord.Embed()
+        embed.add_field(name=header, value=text, inline=True)
+        await bot.delete_message(ctx.message)
+        await bot.say(embed=embed)
+
+    @bot.command(pass_context=True)
+    async def add(ctx):
+        """Adds two numbers together."""
+        left = float(ctx.message.content.split()[1])
+        right = float(ctx.message.content.split()[2])
+        header = str(left) + ' + ' + str(right)
+        text = str(left + right)
+
+        embed = discord.Embed()
+        embed.add_field(name=header, value=text, inline=True)
+        await bot.delete_message(ctx.message)
+        await bot.say(embed=embed)
+
+    @bot.command(pass_context=True)
+    async def subtract(ctx):
+        """Subtract two numbers."""
+        left = float(ctx.message.content.split()[1])
+        right = float(ctx.message.content.split()[2])
+        header = str(left) + ' - ' + str(right)
+        text = str(left - right)
+
+        embed = discord.Embed()
+        embed.add_field(name=header, value=text, inline=True)
+        await bot.delete_message(ctx.message)
+        await bot.say(embed=embed)
+
+    @bot.command(pass_context=True)
+    async def exponent(ctx):
+        """raises the 1st no. to the exponent of the 2nd no."""
+        number = float(ctx.message.content.split()[1])
+        power = float(ctx.message.content.split()[2])
+        header = str(number) + ' to the power of ' + str(power)
+        text = str(number ** power)
+
+        embed = discord.Embed()
+        embed.add_field(name=header, value=text, inline=True)
+        await bot.delete_message(ctx.message)
+        await bot.say(embed=embed)
+
+    @bot.command(pass_context=True)
+    async def divide(ctx):
+        """divides first number by second number"""
+        left = float(ctx.message.content.split()[1])
+        right = float(ctx.message.content.split()[2])
+        header = str(left) + ' / ' + str(right)
+        text = str(left / right)
+
+        embed = discord.Embed()
+        embed.add_field(name=header, value=text, inline=True)
+        await bot.delete_message(ctx.message)
+        await bot.say(embed=embed)
+
+    @bot.command()
+    async def choose(*choices: str):
+        """randomly chooses between multiple options"""
+        header = 'Bot has randomly chosen...'
+        text = random.choice(choices)
+
+        embed = discord.Embed()
+        embed.add_field(name=header, value=text, inline=True)
+        await bot.say(embed=embed)
+
+    @bot.command()
+    async def magicball():
+        '''Answer a question with a response'''
+
+        responses = [
+            'It is certain',
+            'It is decidedly so',
+            'Without a doubt',
+            'Yes definitely',
+            'You may rely on it',
+            'As I see it, yes',
+            'Most likely',
+            'Outlook good',
+            'Yes',
+            'Signs point to yes',
+            'Reply hazy try again',
+            'Ask again later',
+            'Better not tell you now',
+            'Cannot predict now',
+            'Concentrate and ask again',
+            'Do not count on it',
+            'My reply is no',
+            'My sources say no',
+            'Outlook not so good',
+            'Very doubtful'
+        ]
+
+        random_number = random.randint(0, 19)
+        if random_number >= 0 and random_number <= 9:
+            embed = discord.Embed(color=0x60E87B)
+        elif random_number >= 10 and random_number <= 14:
+            embed = discord.Embed(color=0xECE357)
+        else:
+            embed = discord.Embed(color=0xD55050)
+
+        header = 'Magic ball says...'
+        text = responses[random_number]
+
+        embed.add_field(name=header, value=text, inline=True)
+        await bot.say(embed=embed)
+
+    @bot.command()
+    async def coinflip():
+        '''Flips a coin'''
+
+        random_number = random.randint(1, 1000)
+        if random_number >= 500:
+            text = 'It comes up tails'
+        else:
+            text = 'It comes up heads'
+
+        header = 'Bot has flipped a coin...'
+
+        embed = discord.Embed()
+        embed.add_field(name=header, value=text, inline=True)
+        await bot.say(embed=embed)
+
+    @bot.command(pass_context=True)
+    async def rps(ctx):
+        ''' Play a game of rps '''
+        choice = ctx.message.content.split()[1].lower()
+        options = {'rock': 1, 'paper': 2, 'scissors': 3}
+
+        choice = options.get(choice, 0)
+        bot_choice = random.randint(1, 3)
+
+        if choice == 0:
+            header = 'Error!'
+            text = 'Invalid choice! Try again.'
+        # 1 > 3 > 2 > 1
+        elif choice == 1:
+            if bot_choice == 1:
+                header = 'Tie!'
+                text = 'Bot has chosen rock'
+            elif bot_choice == 2:
+                header = 'You lose!'
+                text = 'Bot has chosen paper'
+            elif bot_choice == 3:
+                header = 'You win!'
+                text = 'Bot has chosen scissors'
+        elif choice == 2:
+            if bot_choice == 1:
+                header = 'You win!'
+                text = 'Bot has chosen rock'
+            elif bot_choice == 2:
+                header = 'Tie!'
+                text = 'Bot has chosen paper'
+            elif bot_choice == 3:
+                header = 'You lose!'
+                text = 'Bot has chosen scissors'
+        elif choice == 3:
+            if bot_choice == 1:
+                header = 'You lose!'
+                text = 'Bot has chosen rock'
+            elif bot_choice == 2:
+                header = 'You win!'
+                text = 'Bot has chosen paper'
+            elif bot_choice == 3:
+                header = 'Tie!'
+                text = 'Bot has chosen scissors'
+
+        embed = discord.Embed()
+        embed.add_field(name=header, value=text, inline=True)
         await bot.say(embed=embed)
 
     @bot.command()
@@ -54,46 +209,6 @@ def initiate_bot():
         text = ' '.join(textstring)
         embed = discord.Embed()
         embed.add_field(name='Repeat', value=text, inline=True)
-        await bot.say(embed=embed)
-
-    @bot.command()
-    async def add(left: float, right: float):
-        """Adds two numbers together."""
-        text = str(left+right)
-        embed = discord.Embed()
-        embed.add_field(name='Add', value=text, inline=True)
-        await bot.say(embed=embed)
-
-    @bot.command()
-    async def subtract(left: float, right: float):
-        ''' Subtracts two numbers '''
-        text = str(left-right)
-        embed = discord.Embed()
-        embed.add_field(name='Subtract', value=text, inline=True)
-        await bot.say(embed=embed)
-
-    @bot.command()
-    async def multiply(left: float, right: float):
-        ''' Multiplies two numbers '''
-        text = str(left*right)
-        embed = discord.Embed()
-        embed.add_field(name='Multiply', value=text, inline=True)
-        await bot.say(embed=embed)
-
-    @bot.command()
-    async def divide(left: float, right: float):
-        ''' Divides two numbers '''
-        text = str(left/right)
-        embed = discord.Embed()
-        embed.add_field(name='Divide', value=text, inline=True)
-        await bot.say(embed=embed)
-
-    @bot.command()
-    async def choose(*choices: str):
-        """Chooses between multiple choices."""
-        text = random.choice(choices)
-        embed = discord.Embed()
-        embed.add_field(name='Choice', value=random.choice(choices), inline=True)
         await bot.say(embed=embed)
 
     @bot.command()
